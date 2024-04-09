07aa931dd85607465b0e8d9ec117e173679f4e69
keerthi vasan <keerthivasan110803@gmail.com>
2024-04-09 08:09:25 +0530

    ran collectstatic command

--- spaceYBillCounter/settings.py
+++ spaceYBillCounter/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-dc-apefaao52)^b3^%^ozz=g1ul2+8cb^cq8uf*-#=ht92$+l^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
